b8e3f31b75abdfb7ace7bfa191342cbf3ac29bca
ashsinghal <ayush.singhal@quovantis.com>
2020-08-21 12:43:34 +0530

    add node modules

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+}).listen(8080)
